908e2e6f0571557cb238f883e5c814f6096d9113
Shariq Mobin <Shariq.Mobin@gmail.com>
2016-07-19 17:13:29 -0700

    wavelets, noise models, etc.

--- complex_sinusoid.py
+++ complex_sinusoid.py
@@ -7,18 +7,25 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import librosa
 import math
 
-x2 = np.sin(np.linspace(-np.pi, np.pi, 201))
-x3 = np.sin(np.linspace(-2*np.pi, 2*np.pi, 201))
-x4 = np.sin(np.linspace(-3*np.pi, 3*np.pi, 201))
-x2 = np.tile(x2, 3)
-x3 = np.tile(x3, 3)
-x4 = np.tile(x4, 3)
-c = x2 + x3 + x4
-plt.plot(c)
-plt.show()
-plt.plot(x2)
-plt.plot(x3)
-plt.plot(x4)
-plt.show()
+def get_sum(x):
+    tot = x[0]
+    for sig in x[1:]:
+        tot += sig[:len(tot)]
+        print 'hi'
+    return tot
 
+x = []
 
+plt.subplot(212)
+plt.title("Indvidiual")
+for i in (440, 446):
+    tmp = np.sin(np.linspace(-np.pi, np.pi, i))
+    x.append(np.tile(tmp, 100))
+    plt.plot(x[-1], label='%d' % i)
+plt.legend()
+
+plt.subplot(211)
+plt.title("Sum")
+plt.plot(get_sum(x))
+
+plt.show()
--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pdb
+from numpy import fft
+import matplotlib.pyplot as plt
 import math
 from math import log, ceil, pi, sqrt
 
@@ -8,7 +11,7 @@ vstep = np.vectorize(step)
 
 def fourier_wavelet(w, w0, var):
     #return 1. / (sqrt(2 * pi * var)) * np.exp(1./2 * (w - w0) ** 2 / var) * vstep(w)
-    return 1. / (pi ** (1./4)) * np.exp(-(w - w0) ** 2 / var) * vstep(w) # FIXME?
+    return np.exp(-(w - w0) ** 2 / var) * vstep(w) # FIXME?
 
 def ln(x):
     return log(x, math.e)
@@ -19,19 +22,31 @@ def log2(x):
 def gauss_mid(mean, var):
     return math.sqrt(var * ln(2)) + mean
 
-def var_for_bw(octave_size):
-    return ((1./8) * octave_size) ** 2 / ln(2)
+def var_for_bw(freq):
+    const = 2 ** (1./6) - 1
+    return (2 * (freq * const) ** 2) / ln(2)
+    #return ((1./12) * freq) ** 2 / ln(2)
+    #freq_factor = (freq * (2 ** (1./6) - 1)) ** 2
+    #sq = math.sqrt(1./2)
+    #return - 1./(freq_factor * ln(sq))
 
-def compute_wavelets(freqs):
-    fmin = 80
-    noctaves = 4
-    nwavelets_per_octave = 32
+def compute_wavelets(freqs, plot=False):
+    fmin = 2 ** 6
+    noctaves = 7
+    nwavelets_per_octave = 12
     nwavelets = noctaves * nwavelets_per_octave
     wavelets = np.zeros((nwavelets, len(freqs)))
 
     for i in range(nwavelets_per_octave * noctaves):
         w0 = fmin * 2 ** (float(i)/nwavelets_per_octave)
         var = var_for_bw(w0)
-        wavelets[i,:] = fourier_wavelet(freqs, w0, var)
+        wavelets[i,:] = (fourier_wavelet(freqs, w0, var)) ** 2
+        if plot:
+            plt.plot(freqs, wavelets[i,:], label=("%d" % i))
+    if plot:
+        sfreq = fft.fftshift(freqs)
+        plt.axis([sfreq[0], sfreq[-1],  0, 1.5])
+        plt.legend()
+        plt.show()
 
     return wavelets
