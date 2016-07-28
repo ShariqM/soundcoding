a9584803cf9edd3e8a7a52c3fe6b1a7f461c9630
Shariq Mobin <Shariq.Mobin@gmail.com>
2016-07-28 11:29:51 -0700

    more refactoring

--- helpers.py
+++ helpers.py
@@ -5,13 +5,6 @@ import matplotlib.pyplot as plt
 import math
 from math import log, ceil, pi, sqrt
 
-def step(x):
-    return 1 * (x > 0)
-vstep = np.vectorize(step)
-
-def fourier_wavelet(w, w0, var):
-    return np.exp(-(w - w0) ** 2 / var) * vstep(w) # FIXME Normalize?
-
 def ln(x):
     return log(x, math.e)
 
@@ -20,13 +13,3 @@ def log2(x):
 
 def mag_angle(wc):
     return np.absolute(wc), np.angle(wc)
-
-def gauss_mid(mean, var):
-    return math.sqrt(var * ln(2)) + mean
-
-def var_for_bw(freq):
-    bw = 1./12 # In harmonic units
-    const = 2 ** (bw * 1./2) - 1
-    return (2 * (freq * const) ** 2) / ln(2)
-
-
--- transform.py
+++ transform.py
@@ -3,6 +3,18 @@ from numpy import fft
 import matplotlib.pyplot as plt
 from helpers import *
 
+def step(x):
+    return 1 * (x > 0)
+vstep = np.vectorize(step)
+
+def fourier_wavelet(w, w0, var):
+    return np.exp(-(w - w0) ** 2 / var) * vstep(w) # FIXME Normalize?
+
+def var_for_bw(freq):
+    bw = 1./12 # In harmonic units
+    const = 2 ** (bw * 1./2) - 1
+    return (2 * (freq * const) ** 2) / ln(2)
+
 def compute_wavelets(freqs, plot=False):
     fmin = 2 ** 6
     noctaves = 5
@@ -25,9 +37,9 @@ def compute_wavelets(freqs, plot=False):
     return wavelets
 
 def get_transform(Fs, x):
-    subsample_factor = 2 ** 3
+    subsample_factor = 2 ** 5
     #start, end = 2000, 3000 # For harmonic2
-    start, end = 3000, 5000 # For harmonic2
+    start, end = 0000, 32000 # For harmonic2
 
     N = len(x)
     Fx = fft.fft(x)
