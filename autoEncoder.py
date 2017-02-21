6fabf5fdfda8fe0f29329f2ffa60c7a70cc033e6
Shariq Mobin <shariq.mobin@gmail.com>
2017-02-21 15:22:52 -0800

    forward pass for AE network

--- autoEncoder.py
+++ autoEncoder.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import pdb
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Network Parameters
 N = 1000 # Dim of I
-n_input = 1000
+n_input = 300
 n_filters = 48
 n_filter_width = 128
 n_sample_rate = 16000
@@ -25,37 +26,51 @@ class Network(object):
     def step(self, x_input):
         #v_m = tf.conv1d(x_input, self.F, 1, 'SAME') # Add leak?
         # Add Noise
+        with tf.name_scope('neurons'):
 
-        v_rest  = tf.constant([0] * n_filters, name='v_rest')
-        a_spike = tf.constant([1] * n_filters, name='a_spike')
+            v_rest  = tf.constant([0] * n_filters, name='v_rest', dtype=tf.float32)
+            a_rest  = tf.constant([0] * n_filters, name='v_rest', dtype=tf.float32)
+            a_spike = tf.constant([1] * n_filters, name='a_spike', dtype=tf.float32)
 
-        # Trainable?
-        v_m = tf.Variable(tf.zeros(n_filters), trainable=True, name="v_m")
-        a_m = tf.Variable(tf.zeros(n_filters), trainable=True, name="a_m")
+            # Trainable?
+            #import pdb
+            #pdb.set_trace()
+            #t = t[0]
 
-        v_conv = tf.einsum("i,ij->j", x_input, self.F)
-        #v_conv = tf.reduce_sum(tf.mul(x_input, F)) # Dot product
-        #v_conv = tf.conv1d(x_input, self.F, 1, 'SAME')
-        tf.scalar_summary(['v_m'] * n_filters, v_m)
-        tf.scalar_summary(['a_m'] * n_filters, a_m)
+            #with tf.variable_scope("time") as scope:
+            #v_m = tf.Variable(tf.zeros(n_filters), trainable=True, name="v_m")
+            #a_m = tf.Variable(tf.zeros(n_filters), trainable=True, name="a_m")
+                #scope.reuse_variables()
 
-        def spike_rest_op():
-            return tf.tuple((v_m.assign(v_rest),
-                             a_m.assign(a_spike)))
+            v_conv = tf.einsum("i,ij->j", x_input, self.F)
+            #v_conv = tf.reduce_sum(tf.mul(x_input, F)) # Dot product
+            #v_conv = tf.conv1d(x_input, self.F, 1, 'SAME')
+            #tf.scalar_summary(['v_m_%d' % t] * n_filters, v_m)
+            #tf.scalar_summary(['a_m_%d' % t] * n_filters, a_m)
+            #tf.scalar_summary(['v_m_%d' % i for i in range(n_filters)], v_m)
+            #tf.scalar_summary(['a_m_%d' % i for i in range(n_filters)], a_m)
+            #for i in range(n_filters):
+            #tf.scalar_summary(['a_m'] * n_filters, a_m)
+            #tf.scalar_summary(['v_m'] * n_filters, a_m)
+            #tf.scalar_summary(['a_m'] * n_filters, a_m)
 
-        def responding_op():
-            return tf.tuple((v_m.assign(v_conv),
-                            a_m))
+            def spike_rest_op():
+                return tf.tuple((v_m.assign(v_rest),
+                                 a_m.assign(a_spike)))
 
-        _step = tf.case((
-                         #(tf.reshape(tf.greater(v_m, 0.5), [n_filters]), spike_rest_op),
-                         (tf.greater(v_m, 0.5), spike_rest_op),
-                        ),
-                         responding_op)
+            def responding_op():
+                return tf.tuple((v_m.assign(v_conv), a_spike,
+                                a_m))
 
-        return _step
-
-
+            a_m = tf.select(tf.greater(v_conv, 0.5), a_spike, a_rest)
+            v_m = tf.select(tf.greater(v_conv, 0.5), v_rest, v_conv)
+            #_step = tf.case((
+                             #(tf.reshape(tf.greater(v_m, 0.5), [n_filters]), spike_rest_op),
+                             #(tf.greater(v_m, 0.5), spike_rest_op),
+                            #),
+                             #responding_op)
+        #return tf.group(a_m, v_m)
+        return (a_m, v_m)
 
 #batch = np.random.randn(batch_size, n_input, 1)
 data = np.random.randn(n_input)
@@ -63,6 +78,7 @@ data = np.random.randn(n_input)
 with tf.Graph().as_default(), tf.Session() as sess:
     network = Network(n_filter_width, n_filters)
     dataPH = tf.placeholder(tf.float32, shape=[n_filter_width], name="data")
+    #tPH = tf.placeholder(tf.int32, shape=[1], name="t")
 
     step_op = network.step(dataPH)
     summary_op = tf.merge_all_summaries()
@@ -71,14 +87,23 @@ with tf.Graph().as_default(), tf.Session() as sess:
     summary_writer = tf.train.SummaryWriter('AEresults', sess.graph)
 
     sess.run(init_op)
+    aResults = np.zeros((n_filters, (n_input - n_filter_width)))
+    vResults = np.zeros((n_filters, (n_input - n_filter_width)))
     for t in range(n_input - n_filter_width):
+        print ("Step %d" % t)
+        #feed_dict = {tPH: [t], dataPH: data[t:t+n_filter_width]}
         feed_dict = {dataPH: data[t:t+n_filter_width]}
-        sess.run(step_op, feed_dict=feed_dict)
+        a_m, v_m = sess.run(step_op, feed_dict=feed_dict)
+        aResults[:,t] = a_m
+        vResults[:,t] = v_m
 
-        summary_str = sess.run(summary_op, feed_dict=feed_dict)
-        summary_writer.add_summary(summary_str, t)
-        summary_writer.flush()
+        #summary_str = sess.run(summary_op, feed_dict=feed_dict)
+        #summary_writer.add_summary(summary_str, t)
+        #summary_writer.flush()
 
-    x = np.random.randn(n_input)
+    plt.subplot(2,1,1)
+    plt.imshow(aResults, interpolation="nearest")
+    plt.subplot(2,1,2)
+    plt.imshow(vResults, interpolation="nearest")
+    plt.show()
 
-    net = Network(n_filter_width, n_filters)
